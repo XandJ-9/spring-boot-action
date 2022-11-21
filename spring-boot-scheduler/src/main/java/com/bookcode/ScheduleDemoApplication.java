9c5a1a1b4e868531aa8dbbd828a3c9a56fbd01fc
XandJ-9 <j538350@163.com>
2022-11-21 21:32:02 +0800

    提交代码

--- spring-boot-scheduler/src/main/java/com/bookcode/ScheduleDemoApplication.java
+++ spring-boot-scheduler/src/main/java/com/bookcode/ScheduleDemoApplication.java
@@ -4,21 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ScheduledExecutorTask;
-
-import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * Hello world!
- *
  */
 @EnableScheduling // 启用调度注解
 @SpringBootApplication
-public class ScheduleDemoApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class ScheduleDemoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 
         ConfigurableApplicationContext applicationContext = SpringApplication.run(ScheduleDemoApplication.class, args);
     }
