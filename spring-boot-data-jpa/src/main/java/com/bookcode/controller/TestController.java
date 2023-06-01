4937cf8c67f9bd211b7cca7da62f8b688514ab64
xujia <j538350@163.com>
2023-06-01 19:44:38 +0800

    spring-boot-jpa更新分页查询方法的使用

--- spring-boot-data-jpa/src/main/java/com/bookcode/controller/TestController.java
+++ spring-boot-data-jpa/src/main/java/com/bookcode/controller/TestController.java
@@ -7,6 +7,9 @@ import com.bookcode.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,14 +22,14 @@ public class TestController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping("/users/all")
     public List<User> getAllUser() {
         List<User> users = userRepository.findAll();
         log.info(users.toString());
         return users;
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public User getUser(@PathVariable("id") int id) {
         Optional<User> user = userRepository.findById((long) id);
         log.info(user.get().toString());
@@ -40,4 +43,21 @@ public class TestController {
         log.info("add a user: {}", user);
         return user;
     }
+
+    /**
+     * 返回分页查询的结果
+     * @param pageNum
+     * @param size
+     * @return
+     */
+    @GetMapping("/users/pages")
+    public Page<User> selectPage(@RequestParam("pageNum") Integer pageNum, @RequestParam("size") Integer size){
+
+        PageRequest pageRequest = PageRequest.of(pageNum, size);
+
+        Page<User> userPage = userRepository.findAll(pageRequest);
+
+        return userPage;
+    }
+
 }
