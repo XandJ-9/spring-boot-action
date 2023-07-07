5d2224d39541a1a6f1245190b59a9723a99062a8
xujia <j538350@163.com>
2023-07-07 12:06:59 +0800

    调整

--- demo-spring-mvc/src/main/java/com/xujia/HelloWebApplication.java
+++ demo-spring-mvc/src/main/java/com/xujia/HelloWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
@@ -17,13 +18,17 @@ public class HelloWebApplication
         SpringApplication.run(HelloWebApplication.class, args);
     }
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello(){
         return "hello";
     }
 
-    @GetMapping("/home")
+    /**
+     * 多个url地址请求同一个后端方法
+     * @return
+     */
+    @GetMapping(value ={"/home","/hi"})
     public String home(){
         System.out.println("home...");
         return "home";
